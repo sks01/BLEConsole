c2564c5ca41aff4c1dbdff943cb830f26722e126
sks01 <30595987+sks01@users.noreply.github.com>
2021-01-24 00:47:12 -0800

    Updated version

--- BLEConsole/Properties/AssemblyInfo.cs
+++ BLEConsole/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bluetooth Low Energy device console")]
-[assembly: AssemblyDescription("Console application for working with GATT/BLE devices")]
+[assembly: AssemblyTitle("BLEConsole")]
+[assembly: AssemblyDescription("Console application for testing BLE devices")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BLEConsole")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.6.0")]
-[assembly: AssemblyFileVersion("1.5.6.0")]
+[assembly: AssemblyVersion("0.4.0.1")]
+[assembly: AssemblyFileVersion("0.4.0.1")]
